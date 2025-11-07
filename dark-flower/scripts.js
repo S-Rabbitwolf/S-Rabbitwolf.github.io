2946c2b4d4b3e7e3edd28efdd8fe58135b671e98
S <christopher.weston@wsu.edu>
2025-11-07 13:49:27 -0800

    Adjusted fadein from behindtext

--- dark-flower/scripts.js
+++ dark-flower/scripts.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.classList.toggle("darkMode");
 
         // toggling dark mode for boxes as well
-        document.querySelectorAll('.container, .image-box, .description').forEach(function (el) {
+        document.querySelectorAll('.container, .image-box, .description, .BehindText').forEach(function (el) {
             el.classList.toggle("darkMode");
         })
         
